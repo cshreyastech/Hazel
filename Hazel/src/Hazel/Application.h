6ad8fd5fe611575a66467d7a3631af19fef5bdd9
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2021-05-29 08:41:44 -0700

    Our first shader.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -8,6 +8,7 @@
 #include "Hazel/Events/ApplicationEvent.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Hazel/Renderer/Shader.h"
 
 namespace Hazel {
 
@@ -36,6 +37,8 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertextBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
+
 	private:
 		static Application* s_Instance;
 	};
