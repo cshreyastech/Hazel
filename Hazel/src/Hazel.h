14c254aca233d33f67ec8fc5dcf5eca21c21f77b
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2021-06-02 04:44:38 -0700

    Added orthographic camera.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -11,5 +11,16 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 //------Entry Point--------------
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RendererCommand.h"
+
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+//-------------------------------
+
+//------Entry Point--------------
 #include "Hazel/EntryPoint.h"
 //-------------------------------
